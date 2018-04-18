a680c60af8a7cf7ab6a57b66d7ed2312f519e54a
taroteeth <zarahsikora@gmail.com>
2018-04-18 10:54:10 -0400

    cta banner/button

---
+++ wp-content/themes/bloodmoon/components/button.php
@@ -0,0 +1,16 @@
+<?php
+$buttonLabel = get_sub_field('button_label');
+$pathType = get_sub_field('path_type');
+$internalType = get_sub_field('path_type') == 'internal';
+$buttonPath = get_sub_field('button_path');
+$internalPath = get_permalink($buttonPath[0]);
+$buttonUrl = get_sub_field('button_url');
+?>
+
+<div class="btn-wrapper">
+    <button>
+        <a href="<?php echo ($internalType ? $internalPath : $buttonUrl); ?>">
+            <?php echo $buttonLabel; ?>
+        </a>
+    </button>
+</div><!-- .btn-wrapper -->
--- wp-content/themes/bloodmoon/components/cta-banner.php
+++ wp-content/themes/bloodmoon/components/cta-banner.php
@@ -1,1 +1,14 @@
-<?php ?>
+<?php
+$bannerTitle = get_sub_field('banner_title');
+
+?>
+
+<div class="cta-banner">
+    <div class="inner">
+        <?php
+        echo $bannerTitle;
+
+        include('button.php');
+        ?>
+    </div><!-- .inner -->
+</div><!-- .cta-banner -->
