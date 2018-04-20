52a6321be290af61542f1638c590bb7661f16e5c
taroteeth <zarahsikora@gmail.com>
2018-04-20 11:28:35 -0400

    fix post grid styling

--- wp-content/themes/bloodmoon/components/text-module.php
+++ wp-content/themes/bloodmoon/components/text-module.php
@@ -2,6 +2,10 @@
 
 $wysiwygContent = get_sub_field('wysiwyg');
 
-echo $wysiwygContent;
-
 ?>
+
+<div class="text-module">
+    <div class="inner">
+        <?php echo $wysiwygContent; ?>
+    </div><!-- .inner -->
+</div><!-- .text-module -->
