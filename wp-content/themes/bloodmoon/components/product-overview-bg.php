f0a8f2ecf4c0736861e5df86737d7a4d320c8b07
Devon Riley <dev@knowncreative.co>
2018-04-27 13:02:58 -0400

    SVG background fix

--- wp-content/themes/bloodmoon/components/product-overview-bg.php
+++ wp-content/themes/bloodmoon/components/product-overview-bg.php
@@ -1,12 +1,12 @@
-<svg id="product-overview-bg" viewBox="0 0 1920 765">
+<svg id="product-overview-bg" class="svg-background" viewBox="0 0 1920 765">
     <defs>
         <rect id="path-1" x="0" y="0" width="1920" height="716"></rect>
-        <linearGradient x1="48.945767%" y1="2.91590167%" x2="50%" y2="100%" id="linearGradient-3">
+        <linearGradient x1="48.945767%" y1="2.91590167%" x2="50%" y2="100%" id="gradient1">
             <stop stop-color="#FFFFFF" offset="0%"></stop>
             <stop stop-color="#E9F6FF" offset="100%"></stop>
         </linearGradient>
         <rect id="path-4" x="0" y="0" width="318" height="488"></rect>
-        <linearGradient x1="48.945767%" y1="2.91590167%" x2="50%" y2="100%" id="linearGradient-6">
+        <linearGradient x1="48.945767%" y1="2.91590167%" x2="50%" y2="100%" id="gradient2">
             <stop stop-color="#FF385E" offset="0%"></stop>
             <stop stop-color="#E9F6FF" offset="100%"></stop>
         </linearGradient>
@@ -15,18 +15,18 @@
         <g id="Product-Overview">
             <g id="Fill-1">
                 <mask id="mask-2" fill="white">
-                <use xlink:href="#path-1"></use>
+                    <use xlink:href="#path-1"></use>
                 </mask>
                 <g id="Mask"></g>
-                <path d="M407.436692,-650.215684 C921.373337,-751.657122 2360,-819.567758 2360,90.1767413 C2360,1274.56903 939.584335,383.937311 392.322001,395.836544 C-125.041899,407.086456 -400.059367,997.818958 -436.503406,90.1767413 C-472.947444,-817.465475 -106.499952,-548.773711 407.436692,-650.215684 Z" fill="url(#linearGradient-3)" mask="url(#mask-2)"></path>
-                <path d="M407.436692,-787.215684 C921.373337,-888.657122 2360,-956.567758 2360,-46.8232587 C2360,1137.56903 939.584335,246.937311 392.322001,258.836544 C-125.041899,270.086456 -400.059367,860.818958 -436.503406,-46.8232587 C-472.947444,-954.465475 -106.499952,-685.773711 407.436692,-787.215684 Z" id="Fill-1-Copy" fill="url(#linearGradient-3)" style="mix-blend-mode: multiply;" mask="url(#mask-2)" transform="translate(960.500000, -137.500000) rotate(-5.000000) translate(-960.500000, 137.500000) "></path>
+                <path d="M407.436692,-650.215684 C921.373337,-751.657122 2360,-819.567758 2360,90.1767413 C2360,1274.56903 939.584335,383.937311 392.322001,395.836544 C-125.041899,407.086456 -400.059367,997.818958 -436.503406,90.1767413 C-472.947444,-817.465475 -106.499952,-548.773711 407.436692,-650.215684 Z" fill="url(#gradient1)" mask="url(#mask-2)"></path>
+                <path d="M407.436692,-787.215684 C921.373337,-888.657122 2360,-956.567758 2360,-46.8232587 C2360,1137.56903 939.584335,246.937311 392.322001,258.836544 C-125.041899,270.086456 -400.059367,860.818958 -436.503406,-46.8232587 C-472.947444,-954.465475 -106.499952,-685.773711 407.436692,-787.215684 Z" id="Fill-1-Copy" fill="url(#gradient1)" style="mix-blend-mode: multiply;" mask="url(#mask-2)" transform="translate(960.500000, -137.500000) rotate(-5.000000) translate(-960.500000, 137.500000) "></path>
             </g>
             <g id="Group-13" style="mix-blend-mode: multiply;" transform="translate(1703.325786, 285.000000)">
                 <mask id="mask-5" fill="white">
                     <use xlink:href="#path-4"></use>
                 </mask>
                 <g id="Mask"></g>
-                <g mask="url(#mask-5)" fill="url(#linearGradient-6)">
+                <g mask="url(#mask-5)" fill="url(#gradient2)">
                     <g transform="translate(24.000000, -35.000000)">
                         <circle id="Oval-Copy-3" style="mix-blend-mode: multiply;" transform="translate(212.000000, 212.000000) rotate(-24.000000) translate(-212.000000, -212.000000) " cx="212" cy="212" r="160"></circle>
                         <circle id="Oval-Copy-4" style="mix-blend-mode: multiply;" transform="translate(97.000000, 442.000000) rotate(-24.000000) translate(-97.000000, -442.000000) " cx="97" cy="442" r="73"></circle>
