ba285a00e8c8f4c6e009ec837dac1213788a8cb6
taroteeth <zarahsikora@gmail.com>
2018-05-07 12:18:48 -0400

    whitepapers circle fix

--- wp-content/themes/arxspan/components/grid/grid-post.php
+++ wp-content/themes/arxspan/components/grid/grid-post.php
@@ -101,7 +101,11 @@ if($postGrid){ ?>
                     <div class="post-container">
                         <div class="inner">
                             <div class="top">
-                                <svg viewBox="0 0 271 271">
+                                <svg class="circle" viewBox="0 0 200 200">
+                                      <circle cx="100" cy="100" r="100"/>
+                                </svg>
+
+                                <svg class="whitepaper" viewBox="0 0 271 271">
                                     <use xlink:href="#whitepaper-icon"></use>
                                 </svg>
                             </div>
