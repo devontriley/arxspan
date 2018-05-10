e6f4d1fe8690952785c062be93d76e06a62c4a9d
taroteeth <zarahsikora@gmail.com>
2018-05-10 11:44:24 -0400

    aesthetic tweaks

--- wp-content/themes/arxspan/footer.php
+++ wp-content/themes/arxspan/footer.php
@@ -23,7 +23,7 @@
             <ul>
                 <li class="title">Legal</li>
                 <li>Arxspan and Arxlab are registered trademarks of Arxspan, LLC</li>
-                <li><a href="<?php echo get_permalink(102); ?>">Privacy Policy</a></li>
+                <li class="privacy"><a href="<?php echo get_permalink(102); ?>">Privacy Policy</a><svg viewbox="0 0 10 16"><use xlink:href="#button-arrow"></use></svg></li>
                 <li class="trademark">©2018 Arxspan® All Rights Reserved.</li>
             </ul>
         </div><!-- .legal -->
