5b106e57f7c3d903224e57420c3afe63eaae7558
Devon Riley <dev@knowncreative.co>
2018-05-11 14:36:22 -0400

    remove empty p's in shortcode

--- wp-content/themes/arxspan/functions.php
+++ wp-content/themes/arxspan/functions.php
@@ -91,6 +91,15 @@ function button($buttonLabel, $buttonUrl){ // placeholders, can change name if w
 
 // SHORTCODE
 
+add_filter( 'acf/load_value/type=wysiwyg', 'remove_shortcode_p', 10, 3 );
+function remove_shortcode_p( $value, $post_id, $field ) {
+    $content = apply_filters('the_content',$value);
+    $patt = '/<p>&nbsp;<\/p>/';
+    $content = preg_replace( $patt, '', $content );
+
+    return $content;
+}
+
 // wysiwyg cols shortcode
 
 function row($params, $content = null) {
