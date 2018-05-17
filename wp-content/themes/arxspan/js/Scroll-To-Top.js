f43aa93d8a2f8bc1c9ceeab9ff83015de7fc9155
taroteeth <zarahsikora@gmail.com>
2018-05-17 17:10:24 -0400

    scroll to top function improvements

--- wp-content/themes/arxspan/js/Scroll-To-Top.js
+++ wp-content/themes/arxspan/js/Scroll-To-Top.js
@@ -1,8 +1,33 @@
-import { scrollTo } from './helpers';
+import {getScrollPosition, scrollTo} from './helpers';
 
 class scrollToTop {
     constructor(){
         this.scrollTopButton = document.getElementById('scroll-to-top');
+
+        //get height of page
+        var body = document.body,
+            html = document.documentElement;
+
+        var docHeight = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+        var halfHeight = ( (docHeight / 2) - 317 ); // half height does not account for header + footer
+
+        console.log(halfHeight);
+
+        this.interval = setInterval(function(){
+            this.currentScroll = getScrollPosition();
+
+            // show if 50% scroll or greater
+            if (this.currentScroll >= halfHeight){
+                this.scrollTopButton.classList.add('active');
+            }
+
+            // remove button if scroll back to top
+            if (this.currentScroll < halfHeight) {
+                    this.scrollTopButton.classList.remove('active');
+                }
+
+            console.log(this.currentScroll);
+        }.bind(this), 30);
 
         if(this.scrollTopButton){
             this.scrollTopButton.addEventListener('click', function(e) {
