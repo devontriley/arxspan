46fe44011cc2d9619fdde1d79caa0c7ecc18d016
taroteeth <zarahsikora@gmail.com>
2018-05-18 11:40:43 -0400

    remove thumbnail dimensions filter

--- wp-content/themes/arxspan/functions.php
+++ wp-content/themes/arxspan/functions.php
@@ -104,7 +104,10 @@ add_action('admin_init', 'remove_editor');
 // prevent wp from adding inline dimensions to images
 add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
 add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-
+// Genesis framework only
+add_filter( 'genesis_get_image', 'remove_thumbnail_dimensions', 10 );
+// Removes attached image sizes as well
+add_filter( 'the_content', 'remove_thumbnail_dimensions', 10 );
 function remove_thumbnail_dimensions( $html ) {
     $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
     return $html;
